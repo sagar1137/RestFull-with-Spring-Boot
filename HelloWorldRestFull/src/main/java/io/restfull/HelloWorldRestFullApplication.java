8813b896830e8b28ae42e65d7a1884914a69d964
Sagar Patil <Sagar Patil@DESKTOP-HLR597N>
2021-01-28 14:45:42 +0530

    First Commit, Hello World

---
+++ HelloWorldRestFull/src/main/java/io/restfull/HelloWorldRestFullApplication.java
@@ -0,0 +1,13 @@
+package io.restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldRestFullApplication.class, args);
+	}
+
+}
